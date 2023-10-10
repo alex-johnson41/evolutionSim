91f5b23a89fa29a2d7fa77c6fc41d1bd3541558a
alex-johnson41 <alex@heathjohnson.org>
2023-10-10 16:18:50 -0500

    Update main, add basic test

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from sim.sim_controller import SimController
 from sim.types import SurvivalConditions
+from random import seed
+from time import time
+import cProfile
 
 
+start = time()
+
+seed(1)
 controller = SimController(
     population=100, 
-    generation_steps=100, 
-    genome_length=10, 
-    internal_neuron_count=5, 
-    x_size=30, 
-    y_size=30, 
+    generation_steps=300, 
+    genome_length=6, 
+    internal_neuron_count=2, 
+    x_size=100, 
+    y_size=100, 
     survival_condition= SurvivalConditions.RIGHT_SIDE, 
     mutation_chance=0.01)
 controller.setup_simulation()
-controller.run_simulation(100)
+controller.run_simulation(50)
+
+end = time()
+print(f"RUNTIME: {end-start} seconds")
